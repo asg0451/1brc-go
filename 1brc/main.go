e46b6608a66d3d4f39b858c0f095b0e855d0812b
Miles Frankel <miles.frankel@gmail.com>
2024-03-17 18:53:55 -0400

    cl

--- 1brc/main.go
+++ 1brc/main.go
@@ -58,6 +58,7 @@ func main() {
 
 	if err := run(log); err != nil {
 		log.Error("error", "err", err)
+		os.Exit(1)
 	}
 
 	if *memprofile != "" {
@@ -74,21 +75,6 @@ func main() {
 }
 
 const filename = "measurements.txt"
-
-// input:
-// Station name;Temperature
-
-// output:
-// min/max/avg per station, alphabetical order
-// {Abha=-23.0/18.0/59.2, Abidjan=-16.2/26.0/67.3, Abéché=-10.0/29.4/69.0, Accra=-10.1/26.4/66.4, Addis Ababa=-23.7/16.0/67.0, Adelaide=-27.8/17.3/58.5, ...}
-
-// Input value ranges are as follows:
-// Station name: non null UTF-8 string of min length 1 character and max length 100 bytes, containing neither ; nor \n characters. (i.e. this could be 100 one-byte characters, or 50 two-byte characters, etc.)
-// Temperature value: non null double between -99.9 (inclusive) and 99.9 (inclusive), always with one fractional digit
-// There is a maximum of 10,000 unique station names
-// Line endings in the file are \n characters on all platforms
-// Implementations must not rely on specifics of a given data set, e.g. any valid station name as per the constraints above and any data distribution (number of measurements per station) must be supported
-// The rounding of output values must be done using the semantics of IEEE 754 rounding-direction "roundTowardPositive"
 
 type stats struct {
 	min, max, sum, count float64
