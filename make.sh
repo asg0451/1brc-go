47974e7c2d2fe63014b291980e5204f69d1bec4d
Miles Frankel <miles.frankel@gmail.com>
2024-03-17 23:23:45 -0400

    mk

--- make.sh
+++ make.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 echo 'Building...'
-go build -o bin/ .
+go build -o bin/1brc .
 
 echo 'Running...'
 GOGC=off ./bin/1brc >/dev/null # warm cache
 GOGC=off ./bin/1brc -cpuprofile default.pgo >/dev/null
 
 echo 'Building with pgo...'
-go build -o bin/ . # build again with pgo
+go build -o bin/1brc . # build again with pgo
